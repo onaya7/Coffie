bb29d7ac2c46513f7377892ac03d68a38451100a
samuel <samuelayano7@gmail.com>
2023-12-15 15:11:31 +0100

    ref: onboarding page layout

--- lib/pages/onbording.dart
+++ lib/pages/onbording.dart
@@ -10,63 +10,107 @@ class Onboarding extends StatelessWidget {
     return Scaffold(
         body: Column(
       children: [
-        Container(
-            width: w,
-            height: h / 2,
-            color: Colors.amberAccent,
-            child: const Image(
-              image: AssetImage("assets/images/onboarding1.png"),
-              fit: BoxFit.cover,
-            )),
-        Expanded(
-          child: Container(
-            width: w,
-            padding: const EdgeInsets.only(top: 52.0, right: 30.0, left: 30.0),
-            decoration:
-                const BoxDecoration(color: Color.fromARGB(243, 0, 0, 0)),
-            child: Column(
-              children: [
-                const Text(
-                  'Coffee so good, your taste buds will love it.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 34,
-                      fontWeight: FontWeight.w600,
-                      color: Color.fromRGBO(255, 255, 255, 1)),
-                ),
-                const SizedBox(height: 8),
-                const Text(
-                  'The best grain, the finest roast, the powerful flavor.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                      color: Color.fromRGBO(169, 169, 169, 1)),
-                ),
-                const SizedBox(height: 24),
-                SizedBox(
+        Stack(
+          children: [
+            Positioned(
+              child: Container(
                   width: w,
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                        padding: MaterialStateProperty.all(
-                            const EdgeInsets.only(
-                                right: 109, left: 109, top: 21, bottom: 21)),
-                        backgroundColor: MaterialStateProperty.all(
-                            const Color.fromRGBO(198, 124, 78, 1)),
-                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)))),
-                    onPressed: () {},
-                    child: const Text(
-                      'Get Started',
-                      style: TextStyle(
-                          fontSize: 16,
-                          color: Color.fromRGBO(255, 255, 255, 1)),
-                    ),
-                  ),
-                )
-              ],
+                  height: h,
+                  color: const Color.fromARGB(243, 0, 0, 0)),
             ),
-          ),
+            Positioned(
+              child: Container(
+                  width: w,
+                  height: 550,
+                  color: Colors.amberAccent,
+                  child: const Image(
+                    image: AssetImage("assets/images/onboarding1.png"),
+                    fit: BoxFit.cover,
+                  )),
+            ),
+            Positioned(
+              bottom: 10,
+              child: Expanded(
+                child: Container(
+                  width: w,
+                  padding:
+                      const EdgeInsets.only(top: 52.0, right: 30.0, left: 30.0),
+                  decoration: const BoxDecoration(color: Colors.transparent),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Coffee so good,',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 34,
+                            fontWeight: FontWeight.w600,
+                            color: Color.fromRGBO(255, 255, 255, 1)),
+                      ),
+                      const Text(
+                        'your taste buds',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 34,
+                            fontWeight: FontWeight.w600,
+                            color: Color.fromRGBO(255, 255, 255, 1)),
+                      ),
+                      const Text(
+                        'will love it.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 34,
+                            fontWeight: FontWeight.w600,
+                            color: Color.fromRGBO(255, 255, 255, 1)),
+                      ),
+                      const SizedBox(height: 8),
+                      const Text(
+                        'The best grain, the finest roast, the powerful flavor.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: Color.fromRGBO(169, 169, 169, 1)),
+                      ),
+                      const Text(
+                        'powerful flavor.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: Color.fromRGBO(169, 169, 169, 1)),
+                      ),
+                      const SizedBox(height: 24),
+                      SizedBox(
+                        width: w,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                              padding: MaterialStateProperty.all(
+                                  const EdgeInsets.only(
+                                      right: 109,
+                                      left: 109,
+                                      top: 21,
+                                      bottom: 21)),
+                              backgroundColor: MaterialStateProperty.all(
+                                  const Color.fromRGBO(198, 124, 78, 1)),
+                              shape: MaterialStateProperty.all(
+                                  RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(16)))),
+                          onPressed: () {},
+                          child: const Text(
+                            'Get Started',
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Color.fromRGBO(255, 255, 255, 1)),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
         )
       ],
     ));
