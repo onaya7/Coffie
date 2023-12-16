36bb5463230b49c43e74d617ec68a085f6f57231
samuel <samuelayano7@gmail.com>
2023-12-16 05:08:23 +0100

    ref: home screen layout

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -18,47 +18,79 @@ class HomeScreen extends StatelessWidget {
         child: SafeArea(
           child: Row(
             children: <Widget>[
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Location',
-                            style: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w400,
-                                color: Color.fromRGBO(183, 183, 183, 1)),
-                          ),
-                          Row(
-                            children: <Widget>[
-                              const Text(
-                                'Bilzen, Tanjungbalai',
-                                style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w600,
-                                    color: Color.fromRGBO(221, 221, 221, 1)),
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Icon(
-                                  Icons.keyboard_arrow_down,
+              Container(
+                color: Colors.blue,
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                            color: Colors.orange,
+                            padding: const EdgeInsets.only(
+                                left: 30, top: 5, right: 30, bottom: 5),
+                            width: MediaQuery.of(context).size.width,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Expanded(
+                                  
+                                  child: Container(
+                                    color: Colors.yellow,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        const Text(
+                                          'Location',
+                                          style: TextStyle(
+                                              fontSize: 12,
+                                              fontWeight: FontWeight.w400,
+                                              color: Color.fromRGBO(
+                                                  183, 183, 183, 1)),
+                                        ),
+                                        Row(
+                                          children: <Widget>[
+                                            const Text(
+                                              'Bilzen, Tanjungbalai',
+                                              style: TextStyle(
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.w600,
+                                                  color: Color.fromRGBO(
+                                                      221, 221, 221, 1)),
+                                            ),
+                                            IconButton(
+                                              onPressed: () {},
+                                              icon: const Icon(
+                                                Icons.keyboard_arrow_down,
+                                              ),
+                                              iconSize: 30,
+                                              color: const Color.fromRGBO(
+                                                  221, 221, 221, 1),
+                                            )
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
                                 ),
-                                iconSize: 30,
-                                color: const Color.fromRGBO(221, 221, 221, 1),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                      const CircleAvatar(
-                        backgroundImage: AssetImage("assets/images/avatar.png"),
-                      ),
-                    ],
-                  )
-                ],
+                                Container(
+                                  width: 50,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                      color: Colors.pink,
+                                      borderRadius: BorderRadius.circular(14),
+                                      image: const DecorationImage(
+                                          image: AssetImage(
+                                              'assets/images/avatar.png'),
+                                          fit: BoxFit.cover)),
+                                ),
+                              ],
+                            ))
+                      ],
+                    )
+                  ],
+                ),
               ),
             ],
           ),
