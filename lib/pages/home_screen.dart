d02ea9f792ef0318dc53094dc56139e39ce48545
samuel <samuelayano7@gmail.com>
2023-12-16 03:27:23 +0100

    ref: home screen layout and add location and avatar

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -7,14 +7,63 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: 280,
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [Color(0xff131313), Color(0xff303030)],
-          ))),
+        width: MediaQuery.of(context).size.width,
+        height: 280,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [Color(0xff131313), Color(0xff303030)],
+        )),
+        child: SafeArea(
+          child: Row(
+            children: <Widget>[
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Location',
+                            style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400,
+                                color: Color.fromRGBO(183, 183, 183, 1)),
+                          ),
+                          Row(
+                            children: <Widget>[
+                              const Text(
+                                'Bilzen, Tanjungbalai',
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600,
+                                    color: Color.fromRGBO(221, 221, 221, 1)),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.keyboard_arrow_down,
+                                ),
+                                iconSize: 30,
+                                color: const Color.fromRGBO(221, 221, 221, 1),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      const CircleAvatar(
+                        backgroundImage: AssetImage("assets/images/avatar.png"),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/pages/onbording_screen.dart
+++ lib/pages/onbording_screen.dart
@@ -30,88 +30,86 @@ class OnboardingScreen extends StatelessWidget {
             ),
             Positioned(
               bottom: 10,
-              child: Expanded(
-                child: Container(
-                  width: w,
-                  padding: const EdgeInsets.only(
-                      top: 52.0, right: 30.0, bottom: 10.0, left: 30.0),
-                  decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [Color(0x00000000), Colors.black],
-                  )),
-                  child: Column(
-                    children: [
-                      const Text(
-                        'Coffee so good,',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 34,
-                            fontWeight: FontWeight.w600,
-                            color: Color.fromRGBO(255, 255, 255, 1)),
-                      ),
-                      const Text(
-                        'your taste buds',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 34,
-                            fontWeight: FontWeight.w600,
-                            color: Color.fromRGBO(255, 255, 255, 1)),
-                      ),
-                      const Text(
-                        'will love it.',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 34,
-                            fontWeight: FontWeight.w600,
-                            color: Color.fromRGBO(255, 255, 255, 1)),
-                      ),
-                      const SizedBox(height: 8),
-                      const Text(
-                        'The best grain, the finest roast, the',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                            color: Color.fromRGBO(169, 169, 169, 1)),
-                      ),
-                      const Text(
-                        'powerful flavor.',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                            color: Color.fromRGBO(169, 169, 169, 1)),
-                      ),
-                      const SizedBox(height: 24),
-                      SizedBox(
-                        width: w,
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                              padding: MaterialStateProperty.all(
-                                  const EdgeInsets.only(
-                                      right: 109,
-                                      left: 109,
-                                      top: 21,
-                                      bottom: 21)),
-                              backgroundColor: MaterialStateProperty.all(
-                                  const Color.fromRGBO(198, 124, 78, 1)),
-                              shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(16)))),
-                          onPressed: () => Navigator.pushNamed(context, '/home'),
-                          child: const Text(
-                            'Get Started',
-                            style: TextStyle(
-                                fontSize: 16,
-                                color: Color.fromRGBO(255, 255, 255, 1)),
-                          ),
+              child: Container(
+                width: w,
+                padding: const EdgeInsets.only(
+                    top: 52.0, right: 30.0, bottom: 10.0, left: 30.0),
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Color(0x00000000), Colors.black],
+                )),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Coffee so good,',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 34,
+                          fontWeight: FontWeight.w600,
+                          color: Color.fromRGBO(255, 255, 255, 1)),
+                    ),
+                    const Text(
+                      'your taste buds',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 34,
+                          fontWeight: FontWeight.w600,
+                          color: Color.fromRGBO(255, 255, 255, 1)),
+                    ),
+                    const Text(
+                      'will love it.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 34,
+                          fontWeight: FontWeight.w600,
+                          color: Color.fromRGBO(255, 255, 255, 1)),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      'The best grain, the finest roast, the',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: Color.fromRGBO(169, 169, 169, 1)),
+                    ),
+                    const Text(
+                      'powerful flavor.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: Color.fromRGBO(169, 169, 169, 1)),
+                    ),
+                    const SizedBox(height: 24),
+                    SizedBox(
+                      width: w,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            padding: MaterialStateProperty.all(
+                                const EdgeInsets.only(
+                                    right: 109,
+                                    left: 109,
+                                    top: 21,
+                                    bottom: 21)),
+                            backgroundColor: MaterialStateProperty.all(
+                                const Color.fromRGBO(198, 124, 78, 1)),
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.circular(16)))),
+                        onPressed: () => Navigator.pushNamed(context, '/home'),
+                        child: const Text(
+                          'Get Started',
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Color.fromRGBO(255, 255, 255, 1)),
                         ),
-                      )
-                    ],
-                  ),
+                      ),
+                    )
+                  ],
                 ),
               ),
             )
