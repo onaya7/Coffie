03b6fa7cd3dde61000048ac374caae6514cae3ca
samuel <samuelayano7@gmail.com>
2023-12-16 13:24:41 +0100

    ref: home screen layout

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -18,30 +18,23 @@ class HomeScreen extends StatelessWidget {
         child: SafeArea(
           child: Row(
             children: <Widget>[
-              Container(
-                color: Colors.blue,
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Container(
-                            color: Colors.orange,
-                            padding: const EdgeInsets.only(
-                                left: 30, top: 5, right: 30, bottom: 5),
-                            width: MediaQuery.of(context).size.width,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Expanded(
-                                  
-                                  child: Container(
-                                    color: Colors.yellow,
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        const Text(
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                          padding: const EdgeInsets.only(
+                              left: 30, top: 5, right: 30, bottom: 5),
+                          width: MediaQuery.of(context).size.width,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  children: [
+                                    const Row(
+                                      children: <Widget>[
+                                        Text(
                                           'Location',
                                           style: TextStyle(
                                               fontSize: 12,
@@ -49,48 +42,48 @@ class HomeScreen extends StatelessWidget {
                                               color: Color.fromRGBO(
                                                   183, 183, 183, 1)),
                                         ),
-                                        Row(
-                                          children: <Widget>[
-                                            const Text(
-                                              'Bilzen, Tanjungbalai',
-                                              style: TextStyle(
-                                                  fontSize: 14,
-                                                  fontWeight: FontWeight.w600,
-                                                  color: Color.fromRGBO(
-                                                      221, 221, 221, 1)),
-                                            ),
-                                            IconButton(
-                                              onPressed: () {},
-                                              icon: const Icon(
-                                                Icons.keyboard_arrow_down,
-                                              ),
-                                              iconSize: 30,
-                                              color: const Color.fromRGBO(
-                                                  221, 221, 221, 1),
-                                            )
-                                          ],
-                                        ),
                                       ],
                                     ),
-                                  ),
+                                    Row(
+                                      children: <Widget>[
+                                        const Text(
+                                          'Bilzen, Tanjungbalai',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w600,
+                                            color: Color.fromRGBO(
+                                                221, 221, 221, 1),
+                                          ),
+                                        ),
+                                        IconButton(
+                                          onPressed: () {},
+                                          icon: const Icon(
+                                            Icons.keyboard_arrow_down,
+                                          ),
+                                          iconSize: 20,
+                                          color: const Color.fromRGBO(
+                                              221, 221, 221, 1),
+                                        )
+                                      ],
+                                    ),
+                                  ],
                                 ),
-                                Container(
-                                  width: 50,
-                                  height: 50,
-                                  decoration: BoxDecoration(
-                                      color: Colors.pink,
-                                      borderRadius: BorderRadius.circular(14),
-                                      image: const DecorationImage(
-                                          image: AssetImage(
-                                              'assets/images/avatar.png'),
-                                          fit: BoxFit.cover)),
-                                ),
-                              ],
-                            ))
-                      ],
-                    )
-                  ],
-                ),
+                              ),
+                              Container(
+                                width: 50,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(14),
+                                    image: const DecorationImage(
+                                        image: AssetImage(
+                                            'assets/images/avatar.png'),
+                                        fit: BoxFit.cover)),
+                              ),
+                            ],
+                          ))
+                    ],
+                  )
+                ],
               ),
             ],
           ),
